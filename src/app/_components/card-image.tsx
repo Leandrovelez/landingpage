3d6237675343bfeda8b084feaef66f1c99d7be32
Leandro Velez <leandrovelez55@gmail.com>
2025-09-21 18:11:26 -0300

    fix: corrige as imagens

--- src/app/_components/card-image.tsx
+++ src/app/_components/card-image.tsx
@@ -5,18 +5,18 @@ import { useState } from "react"
 export default function CardImage({src, alt, title, fill, ...props}: {src: string, alt: string, title: string, fill?: boolean}) {
     const [loading, setLoading] = useState(true);
     return(
-        <div className='relative overflow-hidden rounded-lg border bg-background py-4 px-2'>
-            <div className='flex h-[180px] flex-col justify-center items-center rounded-md p-2'>
+        <div className='relative overflow-hidden rounded-lg border bg-background py-4 px-2 w-full'>
+            <div className='flex h-[180px] flex-col justify-center items-center rounded-md p-2 w-[800px]'>
                 {fill ? 
-                    <Image src={src} alt={alt} className={`aspect-square rounded-lg object-cover size-40 sm:size-40 md:size-40 
+                    <Image src={src} alt={alt} className={`aspect-square rounded-lg object-cover size-max sm:size-40 md:size-40 
                         ${ loading ? 
                             'scale-110 blur-2xl grayscale' : 
                             'scale-100 blur-0 grayscale-0 transition-all duration-500' 
 
-                        }`} fill 
+                        }`} fill  
                         onLoadingComplete={() => setLoading(false)}/>
                 : 
-                    <Image src={src} alt={alt} className={`aspect-square rounded-lg object-cover size-40 sm:size-40 md:size-40 
+                    <Image src={src} alt={alt} className={`aspect-square rounded-lg object-cover size-max sm:size-40 md:size-40 
                         ${ loading ? 
                             'scale-110 blur-2xl grayscale' : 
                             'scale-100 blur-0 grayscale-0 transition-all duration-500' 
